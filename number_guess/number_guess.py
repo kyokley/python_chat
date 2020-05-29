8a9284f17e03efd42f8b1ce52564e6bf071ba95b
Kevin Yokley <kyokley2@gmail.com>
2020-05-28 22:21:50 -0500

    Handle invalid scenario in number guess game

--- number_guess/number_guess.py
+++ number_guess/number_guess.py
@@ -2,7 +2,20 @@ import random
 import time
 
 
+LOWER = 'L'
+HIGHER = 'H'
+SPOT_ON = 'S'
+
+ACCEPTABLE_RESPONSES = (LOWER,
+                        HIGHER,
+                        SPOT_ON)
+
+
 class InvalidInput(Exception):
+    pass
+
+
+class NoValidGuessesRemaining(Exception):
     pass
 
 
@@ -20,29 +33,39 @@ def main():
 
     done = False
     while not done:
-        resp, guess = make_guess(lower_limit, upper_limit)
+        try:
+            resp, guess = make_guess(lower_limit, upper_limit)
+        except NoValidGuessesRemaining:
+            break
 
-        if resp == 'S':
-            print('We found your number! It was {}'.format(guess))
+        if resp == SPOT_ON:
+            print(f'We found your number! It was {guess}')
             done = True
         else:
             lower_limit, upper_limit = handle_response(resp, guess, lower_limit, upper_limit)
 
 
 def make_guess(lower, upper):
-    guess = random.randint(lower, upper)
-    print('Is your number {}?'.format(guess))
+    try:
+        guess = random.randint(lower, upper)
+    except (UnboundLocalError, ValueError):
+        print("That's funny. I seem to have missed your number. Play again?")
+        raise NoValidGuessesRemaining()
 
-    resp = input('Enter L for lower, H for higher, S for spot on: ').upper().strip()
+    print(f'Is your number {guess}?')
+
+    resp = None
+    while resp not in ACCEPTABLE_RESPONSES:
+        resp = input('Enter L for lower, H for higher, S for spot on: ').upper().strip()
     return resp, guess
 
 
 def handle_response(resp, current_guess, current_lower, current_upper):
-    if resp == 'L':
+    if resp == LOWER:
         # We need to shift our guesses lower
         new_lower = current_lower
         new_upper = current_guess - 1
-    elif resp == 'H':
+    elif resp == HIGHER:
         # We need to shift our guesses higher
         new_lower = current_guess + 1
         new_upper = current_upper
@@ -52,4 +75,7 @@ def handle_response(resp, current_guess, current_lower, current_upper):
 
 
 if __name__ == '__main__':
-    main()
+    try:
+        main()
+    except KeyboardInterrupt:
+        print('Bye!')
--- number_guess/test_number_guess.py
+++ number_guess/test_number_guess.py
@@ -1,5 +1,13 @@
 import pytest
-from number_guess.number_guess import intro, handle_response, InvalidInput
+from number_guess.number_guess import (intro,
+                                       handle_response,
+                                       InvalidInput,
+                                       HIGHER,
+                                       make_guess,
+                                       LOWER,
+                                       SPOT_ON,
+                                       NoValidGuessesRemaining,
+                                       )
 
 
 def test_intro(mocker):
@@ -50,3 +58,29 @@ class TestHandleResponse:
                             self.test_guess,
                             self.current_lower,
                             self.current_upper)
+
+
+class TestMakeGuess:
+    @pytest.fixture(autouse=True)
+    def setUp(self, mocker):
+        self.mock_input = mocker.patch('number_guess.number_guess.input')
+        self.mock_input.return_value = HIGHER
+
+    @pytest.mark.parametrize('user_resp',
+                             [HIGHER, LOWER, SPOT_ON])
+    def test_valid_range(self, user_resp):
+        self.mock_input.return_value = user_resp
+        lower = 3
+        upper = 7
+
+        actual_resp, actual_guess = make_guess(lower, upper)
+        assert actual_resp == user_resp
+        assert lower <= actual_guess <= upper
+
+    @pytest.mark.parametrize('lower,upper',
+                             [(1, 0),
+                              (11, 10),
+                              ])
+    def test_invalid_range(self, lower, upper):
+        with pytest.raises(NoValidGuessesRemaining):
+            make_guess(lower, upper)
