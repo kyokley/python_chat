d33c09fb3e9c5b30d8abb4b9046e1a5465828937
Kevin Yokley <kyokley2@gmail.com>
2020-06-20 16:32:28 -0500

    Clean-up formatting

--- sieve_of_eratosthenes/sieve.py
+++ sieve_of_eratosthenes/sieve.py
@@ -1,14 +1,11 @@
 def get_list(number):
-    
     if not isinstance(number, int):
-        raise Exception('try something else') 
-    
+        raise Exception('try something else')
+
     result = []
-    
 
     if number < 2:
-            raise Exception('passing an integer less than two doesnt work.')
-
+        raise Exception('passing an integer less than two doesnt work.')
 
     for i in range(2, number + 1):  # 0, 1, 2, 3, ..., number
         if number < 2:
@@ -16,21 +13,12 @@ def get_list(number):
 
         result.append(i)
 
-
     return result
 
 
-
-
-
-
-
-
 def remove_multiples(sequence, number):
-   
+
     result = []
-
-
 
     for element in sequence:
 
@@ -46,30 +34,21 @@ def remove_multiples(sequence, number):
 
             result.append(element)
 
-
-
-    return result  
-
-
-
+    return result
 
 
 def prime_list(number):
-    
+
     long_list = get_list(number)
-
-
 
     idx = 0
 
     while idx < len(long_list):
 
         multiple_check = long_list[idx]
-        
+
         long_list = remove_multiples(long_list, multiple_check)
 
         idx = idx + 1
-
-
 
     return long_list
