192ee56b8838cd94e9365a3666b1505e20e0564c
Kevin Yokley <kyokley2@gmail.com>
2020-06-20 14:54:33 -0500

    Adding exceptions

--- sieve_of_eratosthenes/sieve.py
+++ sieve_of_eratosthenes/sieve.py
@@ -1,45 +1,75 @@
 def get_list(number):
+
+    if not isinstance(number, int):
+        raise Exception('try something else')
+
     result = []
+
+
+    if number < 2:
+            raise Exception('passing an integer less than two doesnt work.')
+
 
     for i in range(2, number + 1):  # 0, 1, 2, 3, ..., number
         if number < 2:
             raise Exception('passing an integer less than two doesnt work.')
+
         result.append(i)
 
+
     return result
+
+
+
+
+
+
 
 
 def remove_multiples(sequence, number):
+
     result = []
 
+
+
     for element in sequence:
+
         if element % number == 0 and element > number:
+
             # This is a multiple
+
             pass
+
         else:
+
             # This is not a multiple
+
             result.append(element)
 
-    return result
+
+
+    return element
+
+
+
 
 
 def prime_list(number):
+
     long_list = get_list(number)
 
+
+
     idx = 0
+
     while idx < len(long_list):
+
         multiple_check = long_list[idx]
+
         long_list = remove_multiples(long_list, multiple_check)
+
         idx = idx + 1
 
+
+
     return long_list
-
-
-# [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
-# [2, 3, 5, 7, 9, 11]
-# [2, 3, 5, 7, 11]
-
-
-# remove_multiples([2, 3, 4, 5, 6, 7, 8], 2) => [3, 5, 7]
-# remove_multiples([2, 3, 4, 5, 6, 7, 8], 3) => [2, 4, 5, 7, 8]
-# remove_multiples([2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12], 4) => [2, 3, 5, 6, 7, 9, 10, 11]
